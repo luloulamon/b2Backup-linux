ac39727e783f2c1c37c8eac806793c49ba3ce388
deepfried <louistan3@gmail.com>
2017-09-03 00:51:43 +1200

    updated to do files checks and test encryption of filenames

--- UploadToB2backup.sh
+++ UploadToB2backup.sh
@@ -1,13 +1,53 @@
 #!/bin/bash
 
-exec `dos2unix DirsToBackup.txt`
-dirs=$(<DirsToBackup.txt)
-echo  $dirs 
-for i in $dirs
-do
-	echo $i
-	ls $i
-	find $i -mtime -1 -ls | echo
-done
+initialSync="initialSync.config"
+exec `dos2unix DirsToBackup.config` #convert Windows EOL to Unix
+dirs="DirsToBackup.config"
+
+echo "Checking config files"
+
+#check initialSync file exists
+echo "Checking sync file"
+if [ -f "$initialSync" ]; then 
+	initialSync=$(<$initialSync)
+	if [ -z $initialSync ]; then #check if the file is empty or null
+		echo "Empty Sync file"
+	else
+		echo "Sync File Read"
+	fi
+else 
+	echo "No sync file"
+fi #initialSync check end
+
+#check dirs file exists
+echo "Checking dirs file"
+if [ -f "$dirs" ]; then
+	dirs=$(<$dirs)
+	echo "Dirs loaded"
+else
+	echo "No directory list - touching to create file"
+	touch $dirs
+fi 
+
+if [ $initialSync = 1 ]; then
+	echo "Initial Sync running..."
+	for i in $dirs
+	do
+		files=(`find $i -type f`)
+		test=(`echo \`realpath ${files[0]}\` | openssl -e -base64 -aes-256-cbc -pass file:id_rsa.key`)
+		echo -en "\n"
+		echo ${#files[@]}
+		test2=(`echo $test | openssl -d -base64 -aes-256-cbc -pass file:id_rsa.key`)
+		#realpath 
+	done
+else
+	echo "Starting find files changed..."
+	for i in $dirs
+	do
+		changed=$(find $i -cmin -3600 -type f)
+		echo $changed 
+	done
+fi
+
 
 
