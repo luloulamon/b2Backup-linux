a788e7046b9b50f9fa482b32e10c8659e689326c
deepfriednz <louistan3@gmail.com>
2017-09-18 23:39:00 +1200

    added debug logging
    added file cleanup in tmp

--- UploadToB2backup.sh
+++ UploadToB2backup.sh
@@ -6,76 +6,101 @@ source "client.config" #read config entries from client config file
 accountId=$(cat apikey.config | cut -f1 -d : |  tr -d '[:space:]') #read api credentials from config file
 apiKey=$(cat apikey.config | cut -f2 -d : |  tr -d '[:space:]')
 
+#function to write onto log files for echo statements
 function writeLog {
 
-if [ -t 0 ]
-then
-    data=$1
-	echo "nopipe"
-else
-    data=$(cat)
-	echo $data >> $logFile
-fi
+    if [ -t 0 ]
+    then
+        data=$1
+	    echo "nopipe"
+    else
+        data=$(cat)
+        if [ $DEBUG -eq "1" ]; then #if debugging echo onto stdout as well
+	        echo  $data >> $logFile
+	        echo "DEBUG:: $data"
+	    else
+	        echo  $data >> $logFile
+	    fi
+    fi
 
+}
+
+#function to output debug messages onto logfile as stdout
+function ifDebug {
+    if [ $DEBUG -eq "1" ]; then
+        if [ -t 0 ]
+        then
+            data=$1
+	        echo "No Message"
+        else
+            data=$(cat)
+            echo "DEBUG:: $data" >> $logFile
+	        echo "DEBUG:: $data"
+        fi
+    fi
 }
 
 
 
-if [ $DEBUG == "1" ]; then
-	echo "$dirs"
-	echo "Account ID: $accountId $apiKey"
-	echo `b2 authorize-account $accountId $apiKey`
-fi
 
-echo "Checking config files"
+echo "$dirs" | ifDebug
+echo "Account ID: $accountId $apiKey" | ifDebug
+echo `b2 authorize-account $accountId $apiKey`| ifDebug
+
+
+echo "Checking config files" | writeLog
+
 #check initialSync file exists
-echo "Checking sync file"
+echo "Checking sync file" | writeLog
 if [ -f "$initialSync" ]; then 
 	initialSync=$(<$initialSync)
 	if [ -z $initialSync ]; then #check if the file is empty or null
-		echo "Empty Sync file"
+		echo "Empty Sync file" | writeLog
 	else
-		echo "Sync File Read"
+		echo "Sync File Read" | writeLog
 	fi
 else 
-	echo "No sync file, creating blank sync file"
+	echo "No sync file, creating blank sync file" | writeLog
     touch "$initialSync"
 fi #initialSync check end
 
 #check dirs file exists
-echo "Checking dirs file"
+echo "Checking dirs file" | writeLog
 if [ -f "$dirs" ]; then
 	dirs=$(<$dirs)
-	echo "Dirs loaded"
+	echo "Dirs loaded" | writeLog
 else
-	echo "No directory list - touching to create file, enter directories to back up in this file"
+	echo "No directory list - touching to create file, enter directories to back up in this file" | writeLog
 	touch "$dirs"
 fi 
 
+echo "Done checking config files" | writeLog
+
 if [ -s $intialSync ]; then
-	echo "Initial Sync running..."
-    echo "directory ${#dirs[@]}"
-    #echo ${dirs[1]} 
+	echo "Initial Sync running..." | writeLog
+    echo "directory ${#dirs[@]}" | ifDebug
+    
 	#simplistic encryption of the full path and name to obfuscate the backup names
 	for i in "${dirs[@]}"
 	do
-	    #echo $i
+	    echo "Current item $i" | ifDebug
+
 		files=(`find $i -type f`)
-		
-		#echo ${#files[@]}
+		echo "File list ${#files[@]}" | ifDebug
 		#echo ${files[*]}
 		for j in "${files[@]}"
 		do
 			fullpath=(`realpath $j`)
-			filename=(`echo $fullpath | openssl enc -e -base64 -aes-256-cbc -pass file:id_rsa.pub.key -nosalt | tr -d "/"`) #access array item ${files[0]}
-			echo "Uploading `realpath $j`"
-			#echo $filename
+			filename=(`echo $fullpath | openssl enc -e -base64 -aes-256-cbc -pass file:$key1 -nosalt | tr -d "/"`) #create encrypted filename
+			echo "Uploading $fullpath" | writeLog
+		    echo "Filename $filename" | ifDebug
 			#echo -en "\n"
-			fileChecksum=(`sha1sum $fullpath`)
-	 		openssl enc -e -in $fullpath -out "/tmp/$filename" -aes-256-cbc -pass file:id_rsa.key -nosalt
-			checksum=(`sha1sum /tmp/$filename`) #create a file checksum
-			echo "checksum $checksum"
+			fileChecksum=(`sha1sum $fullpath`) #create filechecksum to add as part of filename
+	 		openssl enc -e -in $fullpath -out "/tmp/$filename" -aes-256-cbc -pass file:$key2 -nosalt #create encrypted file to upload to backblaze
+			checksum=(`sha1sum /tmp/$filename`) #create a file checksum for encrypted file for backblaze upload confirmation
+			echo "Encrypted checksum $checksum" | ifDebug
 			b2 upload-file --sha1 $checksum --threads 4 "$bucketName" "/tmp/$filename" "$filename-$fileChecksum.enc" | writeLog
+			rm -f "/tmp/$filename" #remove encrypted file from /tmp
 			
 			
 		done
